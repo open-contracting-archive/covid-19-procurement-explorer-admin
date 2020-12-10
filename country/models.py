771781be2a85cbd93719e256d44ad3514e845b5f
Bigyan Adhikary <bigyan.adhikary@gmail.com>
2020-12-10 15:50:20 +0545

    Changed Tender data import [x] Changed database structure [x] Validation checks while importing

--- country/models.py
+++ country/models.py
@@ -62,12 +62,21 @@ class CurrencyConversionCache(models.Model):
 
 
 class Supplier(models.Model):
-    supplier_id = models.CharField(verbose_name=_('Supplier ID'), max_length=50, null=True, unique=True)
-    supplier_name = models.CharField(verbose_name=_('Supplier name'), max_length=250)
+    supplier_id = models.CharField(verbose_name=_('Supplier ID'), max_length=50, null=False, unique=True)
+    supplier_name = models.CharField(verbose_name=_('Supplier name'), max_length=250, null=True, blank=True)
+    supplier_address = models.CharField(verbose_name=_('Supplier address'), max_length=250, null=True, blank=True)
 
     def __str__(self):
-        return self.supplier_name
+        return f'{self.supplier_id} - {self.supplier_name}'
 
+
+class Buyer(models.Model):
+    buyer_id = models.CharField(verbose_name=_('Buyer ID'), max_length=50, null=False, unique=True)
+    buyer_name = models.CharField(verbose_name=_('Buyer name'), max_length=250, null=True, blank=True)
+    buyer_address = models.CharField(verbose_name=_('Buyer address'), max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.buyer_id} - {self.buyer_name}'
 
 class Tender(models.Model):
     PROCUREMENT_METHOD_CHOICES = [
@@ -83,25 +92,55 @@ class Tender(models.Model):
         ('canceled', 'canceled'),
     ]
 
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='tenders')
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='suppliers')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='tenders')  
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='tenders', null=True, blank=True)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name='tenders', null=True, blank=True)
+
     contract_id = models.CharField(verbose_name=_('Contract ID'), max_length=50, null=True)
     contract_date = models.DateField(verbose_name=_('Contract date'), null=True)
-    contract_title = models.TextField(verbose_name=_('Contract title'), null=True)
-    contract_value_local = models.FloatField(verbose_name=_('Contract value local'), null=True)
-    contract_value_usd = models.FloatField(verbose_name=_('Contract value USD'),null=True)
-    contract_desc = models.TextField(verbose_name=_('Contract description'), null=True)
     procurement_procedure = models.CharField(verbose_name=_('Procurement procedure'), max_length=25, choices=PROCUREMENT_METHOD_CHOICES, null=True)
     status = models.CharField(verbose_name=_('Contract status'), max_length=25, choices=TENDER_STATUS_CHOICES, null=True)
-    link_to_contract = models.CharField(verbose_name=_('Link to contract'), max_length=250, null=True)
-    link_to_tender = models.CharField(verbose_name=_('Link to tender'), max_length=250, null=True)
-    data_source = models.CharField(verbose_name=_('Data source'), max_length=250, null=True)
+    link_to_contract = models.CharField(verbose_name=_('Link to contract'), max_length=250, null=True, blank=True)
+    link_to_tender = models.CharField(verbose_name=_('Link to tender'), max_length=250, null=True, blank=True)
+    data_source = models.CharField(verbose_name=_('Data source'), max_length=250, null=True, blank=True)
+
     no_of_bidders = models.BigIntegerField(verbose_name=_('Number of Bidders'), null=True, blank=True)
+    contract_title = models.TextField(verbose_name=_('Contract title'), null=True, blank=True)
+    contract_value_local = models.FloatField(verbose_name=_('Contract value local'), null=True, blank=True)
+    contract_value_usd = models.FloatField(verbose_name=_('Contract value USD'),null=True, blank=True)
+    contract_desc = models.TextField(verbose_name=_('Contract description'), null=True, blank=True)
 
     def __str__(self):
-        return self.contract_title
+        return self.contract_id
 
 
+class GoodsServicesCategory(models.Model):
+    category_name = models.CharField(verbose_name=_('Category name'), max_length=50, null=False, unique=True)
+    category_desc = models.TextField(verbose_name=_('Category description'), null=True, blank=True)
 
+    def __str__(self):
+        return self.category_name
+
+
+class GoodsServices(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='goods_services', null=True)
+    goods_services_category = models.ForeignKey(GoodsServicesCategory, on_delete=models.CASCADE, related_name='goods_services', null=True)
+    contract = models.ForeignKey(Tender, on_delete=models.CASCADE, related_name='goods_services', null=True)
+
+    classification_code = models.CharField(verbose_name=_('Classification code'), max_length=25, null=True, blank=True)
+    no_of_bidders = models.BigIntegerField(verbose_name=_('Number of bidders'), null=True, blank=True)
+
+    contract_title = models.TextField(verbose_name=_('Contract title'), null=True, blank=True)
+    contract_desc = models.TextField(verbose_name=_('Contract description'), null=True, blank=True)
+
+    tender_value_local = models.FloatField(verbose_name=_('Tender value local'), null=True, blank=True)
+    tender_value_usd = models.FloatField(verbose_name=_('Tender value USD'),null=True, blank=True)
+    award_value_local = models.FloatField(verbose_name=_('Award value local'), null=True, blank=True)
+    award_value_usd = models.FloatField(verbose_name=_('Award value USD'),null=True, blank=True)
+    contract_value_local = models.FloatField(verbose_name=_('Contract value local'), null=True, blank=True)
+    contract_value_usd = models.FloatField(verbose_name=_('Contract value USD'),null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.goods_services_category} - {self.contract_title}'
 
 
