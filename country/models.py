f1b0fd48d9a0c5e27b00dd356bcb20ff7225f1b8
Bigyan Adhikary <bigyan.adhikary@gmail.com>
2020-12-11 15:31:55 +0545

    [x] Added continent field in Country model

--- country/models.py
+++ country/models.py
@@ -5,10 +5,21 @@ from django.core.validators import RegexValidator, MinValueValidator, MaxValueVa
 
 
 class Country(models.Model):
+    CONTINENT_CHOICES = [
+        ('africa', 'Africa'),
+        ('asia','Asia'),
+        ('europe','Europe'),
+        ('middle_east','Middle East'),
+        ('north_america','North America'),
+        ('south_america','South America'),
+        ('oceanic','Oceanic'),
+    ]
+
     alphaSpaces = RegexValidator(r'^[a-zA-Z ]+$', 'Only letters and spaces are allowed in the Country Name')
 
     slug = models.SlugField(null=True, unique=True)
     name = models.CharField(verbose_name=_('Name'), null=False, unique=True, max_length=50, validators=[alphaSpaces])
+    continent = models.CharField(verbose_name=_('Continent'), max_length=25, choices=CONTINENT_CHOICES, null=True, blank=False)
     population = models.BigIntegerField(verbose_name=_('Population'),null=True, blank=True, validators=[MinValueValidator(0)])
     gdp = models.FloatField(verbose_name=_('GDP'), null=True, blank=True, validators=[MinValueValidator(0)])
     country_code = models.CharField(verbose_name=_('Country code'), max_length=10, null=False)
