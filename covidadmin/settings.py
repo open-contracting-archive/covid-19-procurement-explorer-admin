55421855f6bdad656aa5438d7857a06af6b74998
rubina123 <rubinakarki987@gmail.com>
2020-12-15 15:13:27 +0545

    [x] 'DB_PORT' added in .env.example file [x] 'PORT' key added in settings.py file

--- covidadmin/settings.py
+++ covidadmin/settings.py
@@ -116,7 +116,8 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST')
+        'HOST': env('DB_HOST'),
+        'PORT' : env('DB_PORT')
     }
 }
 
