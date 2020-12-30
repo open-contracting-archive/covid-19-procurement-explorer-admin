41a41c0a5232c956bc6e10efaca396d0afa30636
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2020-12-30 12:25:41 +0000

    deploy-script-update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-ARG1=${1:-main}
-ssh covid19-deployer@covid19admin.py.staging.yipl.com.np 'bash -s' < script.sh $ARG1
+
+ARG1=${1:-deploy}
+ARG2=${1:-main}
+ssh covid19-deployer@covid19admin.py.staging.yipl.com.np 'bash -s' < script.sh $ARG1 $ARG2
--- script.sh
+++ script.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+ARG1=${1:-deploy}
 ARG1=${1:-main}
+
 echo "deploying covid19admin branch=$ARG1"
 cd /home/covid19-deployer/deploy;
-./run.py 'covid19-dev' state.apply pillar='{"python_apps":{"covid19admin":{"git":{"branch":'"$ARG1"'}}}}';
+if [ $ARG1 == 'deploy' ]
+then
+  ./run.py 'covid19-dev' state.apply pillar='{"python_apps":{"covid19admin":{"git":{"branch":'"$ARG2"'}}}}';
+ 
+elif [ $ARG1 == 'command'  ]
+then
+  ./run.py 'covid19-dev' state.sls_id django-custom-command covid19 pillar='{"custom_command":{"script":'"$ARG2"'}}'
+else
+  echo "Example: ./deploy.sh command manage.py command_name or ./deploy.sh deploy branch_name"
+fi
+
 exit;
