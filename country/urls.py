2c04e082fa032066105b24467340f637ac52007b
rubina123 <rubinakarki987@gmail.com>
2020-12-31 15:27:35 +0545

    [x] buyer,country,supplier profile page logic changed for tender list api

--- country/urls.py
+++ country/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('api/v1/country', views.CountryView)
 router.register('api/v1/language', views.LanguageView)
-router.register('api/v1/tender', views.TenderView)
+router.register('api/v1/tender', views.TenderView,basename='TenderView')
 
 urlpatterns = [
     path('', include(router.urls))
