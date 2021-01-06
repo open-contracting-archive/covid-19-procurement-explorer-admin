7440defedb0eedccc0ad87029676d4ce31e881f8
rubina123 <rubinakarki987@gmail.com>
2021-01-06 12:28:23 +0545

    [x] Buyers table api added [x] Suppliers table api added [x] Filter added to these api

--- country/urls.py
+++ country/urls.py
@@ -5,6 +5,8 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register('api/v1/country', views.CountryView)
 router.register('api/v1/language', views.LanguageView)
+router.register('api/v1/buyers', views.BuyerView,basename='BuyerView')
+router.register('api/v1/suppliers', views.SupplierView,basename='SupplierView')
 router.register('api/v1/tender', views.TenderView,basename='TenderView')
 
 urlpatterns = [
