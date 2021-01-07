5f02d780f67c3fa1fb54ad41ade2c8a7d0d925cd
Suyoj Man Tamrakar <suyoj.tamrakar@yipl.com.np>
2021-01-07 16:59:12 +0545

    [x] Changed api endpoint to /api/v1/contracts/ [x] Contract title preferably title. Check as %ilike% in contract title column [x] Country code 2 character uppercase [x] Buyer Id; database table primary key [x] Supplier Id; database table primary key [x] Product Id; database table primary key [x] Contract method; procurement_procedure column in database table; consider the following value set [x] Date range fields preferably as date_from and date_to. check with contract_date column [x] Contract value USD comparison field, preferably as contract_value_usd amount value and value_comparison (string value either gt greater-than or lt less than) [x] Country code changed to country code alpha2

--- country/views.py
+++ country/views.py
@@ -11,6 +11,7 @@ from django.db.models import Avg, Count, Min, Sum, Count,Window
 from .models import Country, Language, Tender,Supplier,Buyer
 from .serializers import CountrySerializer, LanguageSerializer, TenderSerializer ,SupplierSerializer,BuyerSerializer
 from vizualization.views import add_filter_args
+from django_filters import rest_framework as filters
 
 
 class CountryView(viewsets.ModelViewSet):
@@ -50,20 +51,43 @@ class TenderView(viewsets.ModelViewSet):
     serializer_class = TenderSerializer
     ordering_fields = ('contract_title','procurement_procedure','supplier','status','contract_value_usd','buyer')
     filterset_fields = {
-        'country__name': ['exact'],
-        'status': ['exact'],
-        'procurement_procedure': ['exact'],
+        'country__country_code_alpha_2': ['exact'],
     }
 
     def get_queryset(self):
         country =  self.request.GET.get('country',None)
-        buyer = self.request.GET.get('buyer')
-        supplier = self.request.GET.get('supplier')
+        buyer = self.request.GET.get('buyer',None)
+        supplier = self.request.GET.get('supplier',None)
+        product_id = self.request.GET.get('product_id',None)
+        status = self.request.GET.get('status',None)
+        procurement_procedure = self.request.GET.get('procurement_procedure',None)
+        title = self.request.GET.get('title',None)
+        date_from = self.request.GET.get('date_from',None)
+        date_to = self.request.GET.get('date_to',None)
+        contract_value_usd = self.request.GET.get('contract_value_usd',None)
+        value_comparison = self.request.GET.get('value_comparison',None)
         filter_args = {}
+        exclude_args={}
         if country: filter_args['country__country_code_alpha_2'] = country
         if buyer: filter_args = add_filter_args('buyer',buyer,filter_args)
         if supplier: filter_args = add_filter_args('supplier',supplier,filter_args)
-        queryset = Tender.objects.filter(**filter_args)
+        if product_id: filter_args['tenders__goods_services__goods_services_category'] = product_id
+        if title: filter_args['contract_title__iexact'] = title
+        if date_from and date_to : filter_args['contract_date__range'] = [date_from,date_to]
+        if contract_value_usd and value_comparison : 
+            if value_comparison == "gt":
+                filter_args['goods_services__contract_value_usd__gte'] = contract_value_usd
+            elif value_comparison == "lt":
+                filter_args['goods_services__contract_value_usd__lte'] = contract_value_usd
+        if status=="others":
+            exclude_args['status__in'] = ['active','canceled','completed']
+        elif status in ['active','canceled','completed']:
+            filter_args['status'] = status
+        if procurement_procedure=="others":
+            exclude_args['procurement_procedure__in'] = ['open','limited','direct','selective']
+        elif procurement_procedure in ['open','limited','direct','selective']:
+            filter_args['procurement_procedure'] = procurement_procedure
+        queryset = Tender.objects.filter(**filter_args).exclude(**exclude_args)
         return queryset
 
 
