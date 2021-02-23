f5d6c1fbf50a81c6bb7179cf7f0b7ca383b5abaa
Suyoj Man Tamrakar <suyoj.tamrakar@yipl.com.np>
2021-02-23 11:14:51 +0545

    [x] red flag implemented for 7 & 8 [x] Red flag import command edited

--- country/tasks.py
+++ country/tasks.py
@@ -540,10 +540,21 @@ def process_redflag(id):
     red_flag = RedFlags()
     flag1 = getattr(red_flag, 'flag1')(id)
     flag4 = getattr(red_flag, 'flag4')(id)
+    flag8 = getattr(red_flag, 'flag8')(id)
     if flag1:
         flag1_obj = RedFlag.objects.get(function_name='flag1')
         tender.red_flag.add(flag1_obj)
     if flag4:
         flag4_obj = RedFlag.objects.get(function_name='flag4')
         tender.red_flag.add(flag4_obj)
+    if flag8:
+        flag8_obj = RedFlag.objects.get(function_name='flag8')
+        tender.red_flag.add(flag8_obj)
+    print(f'end of {id}')
+
+
+@app.task(name='clear_redflag')
+def clear_redflag(id):
+    tender = Tender.objects.get(id=id)
+    tender.red_flag.clear()
     print(f'end of {id}')
