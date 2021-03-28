d7c73d630b129c8f990598cfac60f6ff28f9ed52
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-03-27 22:49:53 -0400

    ci: Run shellcheck

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 ARG1=${1:-main}
 ARG2=${2:-covid19\-dev}
 echo "deploying covid19admin branch=$ARG1"
-cd /home/covid19-deployer/deploy;
-./run.py "$ARG2" state.apply pillar='{"python_apps":{"covid19admin":{"git":{"branch":'"$ARG1"'}}}}';
-exit;
+cd /home/covid19-deployer/deploy || exit
+./run.py "$ARG2" state.apply pillar='{"python_apps":{"covid19admin":{"git":{"branch":'"$ARG1"'}}}}'
+exit
