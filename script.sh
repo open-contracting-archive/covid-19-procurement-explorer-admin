3ed3a2d49cb9a5385e126a5b2d73a80f5fece6a0
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-03-30 17:19:51 -0400

    chore: Remove deployment scripts that are specific to Young Innovations

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ARG1=${1:-main}
-ARG2=${2:-covid19\-dev}
-echo "deploying covid19admin branch=$ARG1"
-cd /home/covid19-deployer/deploy || exit
-./run.py "$ARG2" state.apply pillar='{"python_apps":{"covid19admin":{"git":{"branch":'"$ARG1"'}}}}'
-exit
