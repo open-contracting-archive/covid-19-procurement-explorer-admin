494f6e2783cdaf6eff196d4142ce0d2dc0d1d586
Suyoj Man Tamrakar <suyoj.tamrakar@yipl.com.np>
2021-04-29 18:24:52 +0545

    CPA-280 - Concentration Red Flag evaluation changed
    [x] Redflag Concentration evaluation changed in tasks
    [x] Redflag 7 reimplemented
    [x] renamed variable

--- country/tasks.py
+++ country/tasks.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import traceback
+from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
 
@@ -482,20 +483,27 @@ def clear_redflag(id):
 
 @app.task(name="process_red_flag7")
 def process_redflag7(id):
-    tenders = Tender.objects.filter(id=id).values(
-        "id", "buyer__buyer_name", "supplier__supplier_name", "supplier__supplier_address"
+    tender = Tender.objects.filter(id=id).values("supplier_id").first()
+    supplier_tenders = list(
+        Tender.objects.filter(supplier=tender["supplier_id"]).values("id", "supplier_id", "buyer_id")
     )
-    flag7_obj = RedFlag.objects.get(function_name="flag7")
-    for tender in tenders:
-        concentration = Tender.objects.filter(
-            buyer__buyer_name=tender["buyer__buyer_name"], supplier__supplier_name=tender["supplier__supplier_name"]
-        )
-        # supplier who has signed X(10) percent or more of their contracts with the same buyer
-        # (wins tenders from the same buyer)
-        if len(concentration) > 10:
-            for i in concentration:
-                obj = Tender.objects.get(id=i.id)
-                obj.red_flag.add(flag7_obj)
+    total_tenders = len(supplier_tenders)
+    if total_tenders > 10:
+        grouped_list = defaultdict(list)
+        for item in supplier_tenders:
+            grouped_list[item["buyer_id"]].append(item)
+        parsed_list = [{"name": key, "data": value, "count": len(value)} for key, value in grouped_list.items()]
+
+        final_list = []
+        for group in parsed_list:
+            if group["count"] > 10 and (((group["count"] / total_tenders) * 100) > 50):
+                for tender in group["data"]:
+                    final_list.append(tender["id"])
+        if len(final_list) > 0:
+            flag7_obj = RedFlag.objects.get(function_name="flag7")
+            # supplier who has signed X(50) percent or more of their contracts with the same buyer
+            # (wins tenders from the same buyer)
+            flag7_obj.tender_set.add(*final_list)
 
 
 @app.task(name="process_red_flag6")
