fd0a61c1b503d49c366d3f2ecbe80098b379b83f
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2021-05-06 15:21:18 +0545

    test-coverage-2021-05-06
    * [x] Command test refactored

--- country/tests/commands/test_process_currency_conversion.py
+++ country/tests/commands/test_process_currency_conversion.py
@@ -1,6 +1,70 @@
 from django.core.management import call_command
 from django.test import TransactionTestCase
 
+from country.models import Buyer, Country, GoodsServices, GoodsServicesCategory, Supplier, Tender
+
+
+def setUpModule():
+    Country.objects.all().delete()
+    country = Country.objects.create(name="Mexico", country_code="MEX", country_code_alpha_2="MX", currency="MXN")
+    category = GoodsServicesCategory.objects.create(
+        category_name="name",
+        category_desc="desc",
+    )
+    supplier = Supplier.objects.create(
+        supplier_id="1",
+        supplier_name="sample supplier",
+        supplier_address="kathmandu",
+    )
+
+    buyer = Buyer.objects.create(
+        buyer_id="1",
+        buyer_name="sample buyer",
+        buyer_address="kathmandu",
+    )
+
+    tender = Tender.objects.create(
+        country=country,
+        supplier=supplier,
+        buyer=buyer,
+        contract_id=1,
+        contract_date="2021-01-01",
+        procurement_procedure="open",
+        status="active",
+        link_to_contract="http://test.com",
+        link_to_tender="http://test.com",
+        data_source="http://test.com",
+        no_of_bidders=1,
+        contract_title="test",
+        contract_value_local=1.0,
+        contract_value_usd=1.0,
+        contract_desc="test description",
+        tender_value_local=1.0,
+        tender_value_usd=1.0,
+        award_value_local=1.0,
+        award_value_usd=1.0,
+    )
+
+    GoodsServices.objects.create(
+        country=country,
+        goods_services_category=category,
+        contract=tender,
+        classification_code="code",
+        no_of_bidders=112,
+        contract_title="title",
+        contract_desc="desc",
+        supplier=supplier,
+        buyer=buyer,
+        quantity_units=1233,
+        ppu_including_vat=1234.0,
+        tender_value_local=1234.0,
+        tender_value_usd=None,
+        award_value_local=1234.0,
+        award_value_usd=None,
+        contract_value_local=1234.0,
+        contract_value_usd=None,
+    )
+
 
 class ProcessCurrencyConversionCommandTest(TransactionTestCase):
     def test_command(self):
--- country/tests/commands/test_save_database_equity.py
+++ country/tests/commands/test_save_database_equity.py
@@ -1,8 +1,21 @@
 from django.core.management import CommandError, call_command
 from django.test import TransactionTestCase
 
+from country.models import Country
+
+
+def setUpModule():
+    Country.objects.all().delete()
+    Country.objects.create(name="Mexico", country_code="MEX", country_code_alpha_2="MX", currency="MXN")
+
 
 class SaveDatabaseEquityCommand(TransactionTestCase):
     def test_command_without_country_batch(self):
         with self.assertRaises(CommandError):
             call_command("save_database_equity")
+
+    def test_command_with_country_batch(self):
+        self.assertEquals(call_command("save_database_equity", "Mexico"), None)
+
+    def test_command_with_wrong_country_batch(self):
+        self.assertEquals(call_command("save_database_equity", "no_country"), None)
