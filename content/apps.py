efe95962a2aaf87beddeb356935006b1c94f37e5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-06-11 13:55:17 -0400

    chore: Add default_auto_field to avoid models.W042 warning

--- content/apps.py
+++ content/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class ContentConfig(AppConfig):
     name = "content"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         import content.signals  # noqa: F401
