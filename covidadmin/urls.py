71d7792728ad25f815aac81e4ef8b20a0dea0ca5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-10 09:51:43 -0500

    urls: Use path instead of re_path

--- covidadmin/urls.py
+++ covidadmin/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
 
 # from wagtail.core import urls as wagtail_urls
@@ -54,5 +54,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        re_path(r"^__debug__/", include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
